636ffc65d5b930de791e68eaba44307a1bd00858
fullstack developer <161649636+stacksculptor@users.noreply.github.com>
2024-11-25 07:52:20 -0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
 				>
 					<img src={logo} alt="logo" className="w-9 h-9 object-contain" />
 					<p className="text-white text-[15.5px] font-bold cursor-pointer flex">
-						Kristian&nbsp;
+						Larry&nbsp;
 						<span className="sm:block hidden">| Web Developer</span>
 					</p>
 				</Link>
